4e3def32cdc34ea6225f90431cbbd527bb1a57d1
tuenguyen <tue.nguyen.2411@gmail.com>
2021-06-17 23:22:44 +0700

    add new CI and add assigner & coder_box

--- mmdet_planing/core/bbox/__init__.py
+++ mmdet_planing/core/bbox/__init__.py
@@ -1,2 +1,10 @@
 from .iou_calculators import *
-from .core import *
+from .coder import  (BaseBBoxCoder, DeltaXYWHBBoxCoder)
+from .builder import build_bbox_coder, build_assigner
+from .assigners import *
+__all__ = [
+    'bbox_overlaps', 'BboxOverlaps2D', 
+    'build_bbox_coder', 'BaseBBoxCoder',
+    'DeltaXYWHBBoxCoder',
+    'MaxIoUAssigner','BaseAssigner','AssignResult','build_assigner'
+]
